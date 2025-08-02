ac4c0e1512df7f0424724e50ed697f1f64316bbf
Cavin <cavinlarry001@gmail.com>
2025-08-02 18:56:19 +0300

    updated a test

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,75 +1,23 @@
 package com.example
 
-import com.example.model.Priority
 import com.example.model.TaskRepository
-import com.example.model.tasksAsTable
-import io.ktor.http.*
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+
+    val taskRepository = TaskRepository()
+
     routing {
-        get("/") {
-            call.respondText { "Hello World" }
-        }
+        staticResources("/", "static")
 
-        // GET all tasks
+        // GET tasks
         get("/tasks") {
-            val tasks = TaskRepository.allTasks()
-            call.respondText(
-                contentType = ContentType.parse("text/html"),
-                text = tasks.tasksAsTable()
-            )
-
-            // GET tasks by their name
-            get("/name/{name}") {
-                val name = call.parameters["name"]
-                if (name == null) {
-                    call.respond(HttpStatusCode.BadRequest)
-                    return@get
-                }
-
-                try {
-                    TaskRepository.taskByName(name)
-                    call.respond(HttpStatusCode.OK)
-                    call.respondText(
-                        contentType = ContentType.parse("text/html"),
-                        text = """
-                            <p>Task Name: $name</p>
-                        """.trimIndent()
-                    )
-                } catch (e: IllegalStateException) {
-                    call.respond(HttpStatusCode.BadRequest, e)
-                }
-            }
-
-            // GET task(s) by their priority
-            get("/priority/{priority}") {
-                val priorityAsText = call.parameters["priority"]
-
-                if (priorityAsText == null) {
-                    call.respond(HttpStatusCode.BadRequest)
-                    return@get
-                }
-
-                try {
-                    val priority = Priority.valueOf(priorityAsText)
-                    val tasks = TaskRepository.tasksByPriority(priority)
-
-                    if (tasks.isEmpty()) {
-                        call.respond(HttpStatusCode.NotFound)
-                        return@get
-                    }
-
-                    call.respondText(
-                        contentType = ContentType.parse("text/html"),
-                        text = tasks.tasksAsTable()
-                    )
-                } catch (e: IllegalArgumentException) {
-                    call.respond(HttpStatusCode.BadRequest)
-                }
-            }
+            val allTasks = taskRepository.getAllTasks()
+            call.respond(HttpStatusCode.OK, allTasks.joinToString("\n"))
         }
     }
 }
--- src/main/kotlin/model/TaskRepository.kt
+++ src/main/kotlin/model/TaskRepository.kt
@@ -1,29 +1,13 @@
 package com.example.model
 
-object TaskRepository {
-    private val tasks = mutableListOf(
+class TaskRepository {
+    val tasks = mutableListOf(
         Task("cleaning", "Clean the house", Priority.Low),
         Task("gardening", "Mow the lawn", Priority.Medium),
         Task("shopping", "Buy the groceries", Priority.High),
-        Task("painting", "Paint the fence", Priority.Medium),
+        Task("painting", "Paint the fence", Priority.Medium)
     )
 
     // fetch all tasks
-    fun allTasks(): List<Task> = tasks
-
-    // fetch a task(s) by priority
-    fun tasksByPriority(priority: Priority) = tasks.filter { it.priority == priority }
-
-    // fetch a task by its name
-    fun taskByName(name: String) {
-        tasks.find { it.name.equals(name, ignoreCase = true) }
-    }
-
-    // add a new task
-    fun addTask(task: Task) {
-        if (task == taskByName(task.name)) {
-            throw IllegalArgumentException("Task with name ${task.name} already exists") // tasks are unique so no duplicates
-        }
-        tasks.add(task)
-    }
+    fun getAllTasks(): List<Task> = tasks
 }
